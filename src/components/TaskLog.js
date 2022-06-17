1f88a97ab1aa1b2b3e0b6460ed0af0d3c2c2b73e
Yolanda Morris <y.mijoe7145@gmail.com>
2022-06-17 16:39:22 -0400

    Second update to TaskLog

--- src/components/TaskLog.js
+++ src/components/TaskLog.js
@@ -6,21 +6,31 @@ const TaskLog = () => {
   useEffect(() => {
     fetchTaskers();
   },[]);
-
+  
   const [taskers, setTaskers] = useState([]);
-
-
+  const [todos, setTodos] = useState([]);
+  
+  
   const fetchTaskers = async () => {
     const data = await fetch( 'http://localhost:4000/taskers' );
-
+    
     const taskers = await data.json();
     console.log(taskers);
     setTaskers(taskers);
-
+    
+    // const todos = taskers.map(tasker => <li>{ tasker }</li>)
+     
+    
   }
-
+  
   return (
-    <div>TaskLog</div>
+    <div>
+      <h1>Task Log</h1>
+      {taskers.map(tasker =>(
+        <h2 key={ tasker.id }>{ tasker.date }</h2> 
+        ))}
+      { todos }
+    </div>
   )
 }
 

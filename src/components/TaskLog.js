df20a312b526fea9159b5651cffdec70450dcc87
Yolanda Morris <y.mijoe7145@gmail.com>
2022-06-17 16:03:38 -0400

    Updates to TaskLog for fetch

--- src/components/TaskLog.js
+++ src/components/TaskLog.js
@@ -1,6 +1,24 @@
-import React from 'react';
+// import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 const TaskLog = () => {
+  useEffect(() => {
+    fetchTaskers();
+  },[]);
+
+  const [taskers, setTaskers] = useState([]);
+
+
+  const fetchTaskers = async () => {
+    const data = await fetch( 'http://localhost:4000/taskers' );
+
+    const taskers = await data.json();
+    console.log(taskers);
+    setTaskers(taskers);
+
+  }
+
   return (
     <div>TaskLog</div>
   )
